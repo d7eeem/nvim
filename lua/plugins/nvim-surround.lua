42dc08f805384571d802906367475342c85844b5
id7xyz <id7xyz@gmail.com>
2025-07-14 15:10:49 +0300

    Lua

--- lua/plugins/nvim-surround.lua
+++ lua/plugins/nvim-surround.lua
@@ -1,10 +1,16 @@
 return {
   "kylechui/nvim-surround",
-  version = "*",   -- Use for stability; omit to use `main` branch for the latest features
+  version = "*", -- Use for stability; omit to use `main` branch for the latest features
   event = "VeryLazy",
   config = function()
     require("nvim-surround").setup({
-      -- Configuration here, or leave empty to use defaults
+      ["$"] = {
+        add = function()
+          return { '"$', '"' }
+        end,
+        find = '"%$.-"',
+        delete = '^"$().-"$',
+      }
     })
   end
 }
