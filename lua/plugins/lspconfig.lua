e6ef714cb535fa6eecca88bc3902549ddf3ac787
d7eeem <id7xyz@gmail.com>
2025-12-23 21:47:46 +0300

    nvim breaking changes

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,128 +1,127 @@
 return {
-  "neovim/nvim-lspconfig",
-  event = {"BufReadPre", "BufNewFile"},
-  dependencies = {
-    "hrsh7th/cmp-nvim-lsp",
-    {"antosha417/nvim-lsp-file-operations", config = true},
-  },
-  config = function()
-    local cmp_nvim_lsp = require("cmp_nvim_lsp")
-    local capabilities = cmp_nvim_lsp.default_capabilities()
-    
-    -- Store the original virtual text config for toggling
-    local original_virtual_text = {
-      spacing = 10,
-      prefix = "●",
-    }
-    
-    vim.diagnostic.config({
-      virtual_text = original_virtual_text,
-      signs = true,
-      underline = false,
-      update_in_insert = false,
-      severity_sort = true,
-    })
-    
-    -- Toggle virtual text function
-    local function toggle_virtual_text()
-      local current_config = vim.diagnostic.config()
-      if current_config.virtual_text then
-        vim.diagnostic.config({virtual_text = false})
-        print("Virtual text disabled")
-      else
-        vim.diagnostic.config({virtual_text = original_virtual_text})
-        print("Virtual text enabled")
-      end
-    end
-    
-    -- Set up the keymap for toggling virtual text
-    vim.keymap.set("n", "<leader>lv", toggle_virtual_text, {desc = "Toggle virtual text"})
-    
-    -- Diagnostic signs (gutter icons)
-    local signs = {Error = " ", Warn = " ", Hint = "󰠠 ", Info = " "}
-    for type, icon in pairs(signs) do
-      local hl = "DiagnosticSign" .. type
-      vim.fn.sign_define(hl, {text = icon, texthl = hl, numhl = ""})
-    end
-    
-    -- Setup language servers using new vim.lsp.config API
-    local servers = {
-      html = {},
-      ts_ls = {},
-      cssls = {
-        filetypes = {"css", "scss", "rasi"},
-      },
-      yamlls = {
-        filetypes = {"yaml", "yml"},
-        settings = {
-          yaml = {
-            keyOrdering = false,
-            format = {enable = true},
-            validate = true,
-            schemaStore = {
-              enable = true,
-              url = "https://www.schemastore.org/api/json/catalog.json",
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+        "hrsh7th/cmp-nvim-lsp",
+        { "antosha417/nvim-lsp-file-operations", config = true },
+    },
+    config = function()
+        local cmp_nvim_lsp = require("cmp_nvim_lsp")
+        local capabilities = cmp_nvim_lsp.default_capabilities()
+
+        local original_virtual_text = {
+            spacing = 10,
+            prefix = "●",
+        }
+
+        vim.diagnostic.config({
+            virtual_text = original_virtual_text,
+            signs = {
+                text = {
+                    [vim.diagnostic.severity.ERROR] = "🛇",
+                    [vim.diagnostic.severity.WARN] = "⚠",
+                    [vim.diagnostic.severity.HINT] = "󰠠",
+                    [vim.diagnostic.severity.INFO] = "🛈",
+                },
             },
-          },
-        },
-      },
-      tailwindcss = {},
-      graphql = {
-        filetypes = {"graphql", "gql", "svelte", "typescriptreact", "javascriptreact"},
-      },
-      emmet_ls = {
-        filetypes = {
-          "html",
-          "typescriptreact",
-          "javascriptreact",
-          "css",
-          "sass",
-          "scss",
-          "less",
-          "svelte",
-        },
-      },
-      jsonls = {
-        filetypes = {"json", "jsonc"},
-        settings = {
-          json = {
-            schemas = require("schemastore").json.schemas(),
-            validate = {enable = true},
-          },
-        },
-      },
-      pyright = {},
-      taplo = {
-        filetypes = {"toml"},
-      },
-      bashls = {
-        filetypes = {"bash", "sh"},
-      },
-      lua_ls = {
-        settings = {
-          Lua = {
-            diagnostics = {globals = {"vim"}},
-            workspace = {
-              library = {
-                [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-                [vim.fn.stdpath("config") .. "/lua"] = true,
-              },
+            underline = false,
+            update_in_insert = false,
+            severity_sort = true,
+        })
+
+        local function toggle_virtual_text()
+            local current_config = vim.diagnostic.config()
+            if current_config.virtual_text then
+                vim.diagnostic.config({ virtual_text = false })
+                print("Virtual text disabled")
+            else
+                vim.diagnostic.config({ virtual_text = original_virtual_text })
+                print("Virtual text enabled")
+            end
+        end
+
+        vim.keymap.set("n", "<leader>lv", toggle_virtual_text, { desc = "Toggle virtual text" })
+
+        local servers = {
+            html = {},
+            ts_ls = {},
+            cssls = {
+                filetypes = { "css", "scss", "rasi" },
             },
-          },
-        },
-      },
-    }
-    
-    -- Configure and enable each server
-    for server, config in pairs(servers) do
-      -- Merge capabilities into the config
-      config.capabilities = capabilities
-      
-      -- Set the configuration for the server
-      vim.lsp.config[server] = config
-      
-      -- Enable the LSP server
-      vim.lsp.enable(server)
-    end
-  end,
+            yamlls = {
+                filetypes = { "yaml", "yml" },
+                settings = {
+                    yaml = {
+                        keyOrdering = false,
+                        format = { enable = true },
+                        validate = true,
+                        schemaStore = {
+                            enable = true,
+                            url = "https://www.schemastore.org/api/json/catalog.json",
+                        },
+                    },
+                },
+            },
+            tailwindcss = {},
+            graphql = {
+                filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
+            },
+            emmet_ls = {
+                filetypes = {
+                    "html",
+                    "typescriptreact",
+                    "javascriptreact",
+                    "css",
+                    "sass",
+                    "scss",
+                    "less",
+                    "svelte",
+                },
+            },
+            jsonls = {
+                filetypes = { "json", "jsonc" },
+                settings = {
+                    json = {
+                        schemas = require("schemastore").json.schemas(),
+                        validate = { enable = true },
+                    },
+                },
+            },
+            pyright = {},
+            taplo = {
+                filetypes = { "toml" },
+            },
+            bashls = {
+                filetypes = { "bash", "sh" },
+            },
+            lua_ls = {
+                settings = {
+                    Lua = {
+                        runtime = {
+                            version = "LuaJIT",
+                        },
+                        diagnostics = {
+                            globals = { "vim" },
+                        },
+                        workspace = {
+                            library = {
+                                [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                                [vim.fn.stdpath("config") .. "/lua"] = true,
+                            },
+                            checkThirdParty = false,
+                        },
+                        telemetry = {
+                            enable = false,
+                        },
+                    },
+                },
+            },
+        }
+
+        for server, config in pairs(servers) do
+            config.capabilities = capabilities
+            vim.lsp.config[server] = config
+            vim.lsp.enable(server)
+        end
+    end,
 }
